f0c51bab3914b7dd22ebff2604ebb90bdc95382d
Salvatore Augusto Lanieri <145227355+salvlan@users.noreply.github.com>
2023-10-02 13:42:44 -0400

    Create 1 - Sum of two numbers.c

---
+++ Collection/1 - Sum of two numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    int n1, n2, ris;
+
+    printf("Dammi il primo numero\n\n");
+    scanf("%d",&n1);
+
+    printf("\nDammi il secondo numero\n\n");
+    scanf("%d",&n2);
+
+    ris=n1+n2;
+
+    printf("\nIl risultato e %d\n\n",ris);
+
+    system("PAUSE");
+
+}
